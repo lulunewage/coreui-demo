942a67392366d8154d99a5343bb46b705a039d0b
luilui3356 <lulunewage@gmail.com>
2019-08-14 17:03:13 +0800

    完善css串接假資料

--- src/views/Common/SearchDate.js
+++ src/views/Common/SearchDate.js
@@ -1,44 +1,53 @@
-import React from 'react';
-import DatePicker from "react-datepicker";
-
+import React from 'react'; 
+import DatePicker from "react-datepicker"; 
 import "react-datepicker/dist/react-datepicker.css";
-import s from './SearchDateStyle.scss';
+import s from './Style.module.scss';
 
-class SearchDate extends React.Component {
-    // super()是呼叫父層的建構式來綁定
-    // this.state可以設置元件本身各別的變數
-    // state是元件自身的變數，props是上面傳下來的變數，
-    // 只要state和props的數值有改變,render都會重新跑一次
-
-    constructor(props){
-        super(props);
-        this.state = {
-            startDate: new Date(),
-            endDate: new Date(),
-        }
-
-    }
-    handleChange=(key,date)=>{
-        this.setState({
-            [key]: date
-        });
-    }
+class SearchDate extends React.Component { 
+    // super()是呼叫父層的建構式來綁定 
+    // this.state可以設置元件本身各別的變數 
+    // state是元件自身的變數，props是上面傳下來的變數， 
+    // 只要state和props的數值有改變,render都會重新跑一次 
     
-    // return回一個標籤，React中的js會用{}包起來，class會變成className，單標籤可以用/>直接結尾
-    render() {
-        return (
-            <span>
-                搜寻日期
-                <DatePicker
-                selected={this.state.startDate}
+    constructor(props){ 
+        super(props); 
+        this.state = { 
+            startDate: new Date(), 
+            endDate: new Date(), 
+        } 
+        
+    } 
+    handleChange=(key,date)=>{ 
+        this.setState({ 
+            [key]: date 
+        }); 
+    } 
+    
+    handleSubmit = e => { 
+        e.preventDefault(); 
+        this.props.callApi(); 
+    } 
+    
+    // return回一個標籤，React中的js會用{}包起來，class會變成className，單標籤可以用/>直接結尾 
+    render() { 
+        return ( 
+            <form onSubmit={this.handleSubmit} className={s.SearchStyle}>
+                <DatePicker 
+                selected={this.state.startDate} 
                 onChange={this.handleChange.bind(this, 'startDate')}
-                />-
-                <DatePicker
-                selected={this.state.endDate}
+                className="form-control" 
+                />- 
+                <DatePicker 
+                selected={this.state.endDate} 
                 onChange={this.handleChange.bind(this, 'endDate')}
+                className="form-control"  
                 />
-            </span>
-        )
-    }
-}
-export default SearchDate;
+            <div className="mb-3 mb-xl-0 col-sm-4 col-md-1 col-sm">
+                <button className="btn btn-primary btn-block">search</button>
+            </div>
+            </form> 
+            
+            ) 
+        } 
+    } 
+    export default SearchDate;
--- src/views/Recharge/Recharge.js
+++ src/views/Recharge/Recharge.js
@@ -1,71 +1,172 @@
-import React, { Component } from 'react';
-import TableData from '../Common/TableData';
-import SearchDate from '../Common/SearchDate';
-import SearchButton from '../Common/SearchButton';
-import s from '../Common/SearchDateStyle.scss';
-import { Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row } from 'reactstrap';
+import React, { Component } from 'react'; 
+import TableData from '../Common/TableData'; 
+import SearchDate from '../Common/SearchDate'; 
+import { Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row } from 'reactstrap'; 
 
-
-export default class Recharge extends Component {
-  constructor(){
-    super();
-    this.state={
-      list:[]
-    }
+let prev = 0; 
+let next = 0; 
+let last = 0; 
+let first = 0; 
+export default class Recharge extends Component { 
+  constructor(){ 
+    super(); 
+    this.state={ 
+      list:[], 
+      currentPage: 1, 
+      todosPerPage: 10, 
+    } 
+    this.handleClick = this.handleClick.bind(this); 
+    this.handleLastClick = this.handleLastClick.bind(this); 
+    this.handleFirstClick = this.handleFirstClick.bind(this); 
   }
-    handleSubmit = e =>{
-        e.preventDefault();
-        // this.props.callApi();
-    }
-    callApi = () =>{
-        fetch(`https://randomuser.me/api/?page={1}}&results={10}`)
-        .then(res =>{
-            return res.json()
-        })
-        .then(json =>{
-          this.handleSetList(json.results);
-        })
-    }
-    handleSetList = newList =>{
-      this.setState({
-        list: newList
-      })
-    }
 
-    render() {
-        return (
-        <div className="animated fadeIn">
-        <Row>
-            <Col>
-            <Card>
-                <CardHeader>
-                <i className="fa fa-align-justify"></i> 充值记录
-                </CardHeader>
-                <CardBody>
-                <form onSubmit={this.handleSubmit} className={s.style}>
-                    <SearchDate />
-                    <SearchButton />
-                </form>
-                
-                <TableData list={this.state.list} />
-                <nav>
-                    <Pagination>
-                    <PaginationItem><PaginationLink previous tag="button">Prev</PaginationLink></PaginationItem>
-                    <PaginationItem active>
-                        <PaginationLink tag="button">1</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem><PaginationLink tag="button">2</PaginationLink></PaginationItem>
-                    <PaginationItem><PaginationLink tag="button">3</PaginationLink></PaginationItem>
-                    <PaginationItem><PaginationLink tag="button">4</PaginationLink></PaginationItem>
-                    <PaginationItem><PaginationLink next tag="button">Next</PaginationLink></PaginationItem>
-                    </Pagination>
-                </nav>
-                </CardBody>
-            </Card>
-            </Col>
-        </Row>
-        </div>
+  callApi = () =>{ 
+    const currentPage = this.state.page; 
+    const todosPerPage = this.state.size; 
+    const indexOfLastTodo = currentPage * todosPerPage; 
+    const indexOfFirstTodo = indexOfLastTodo - todosPerPage; 
+    // const currentTodos = list.slice(indexOfFirstTodo, indexOfLastTodo); 
+    prev = currentPage > 0 ? (currentPage -1) :0; 
+    // last = Math.ceil(list.length/todosPerPage); 
+    next = (last === currentPage) ?currentPage: currentPage +1; 
 
-        )
+    fetch(`https://randomuser.me/api/?page=${currentPage}}&results=${todosPerPage}`) 
+    .then(res =>{ 
+      return res.json() 
+    }) 
+    .then(json =>{ 
+      this.handleSetList(json.results); 
+    }) 
+  } 
+  handleSetList = newList =>{ 
+    this.setState({ 
+      list: newList 
+    }) 
+  } 
+  
+  handleClick(event) { 
+    event.preventDefault(); 
+    this.setState({ 
+      currentPage: Number(event.target.id)
+    });
+    this.callApi();
+  } 
+  
+  handleLastClick(event) { 
+    event.preventDefault(); 
+    this.setState({ 
+      currentPage:last 
+    });
+    this.callApi();
+  } 
+  handleFirstClick(event) { 
+    event.preventDefault(); 
+    this.setState({ 
+      currentPage:1 
+    }); 
+    this.callApi();
+  } 
+  
+  render() { 
+    let { todos, currentPage, todosPerPage } = this.state; 
+    
+    // Logic for displaying current todos 
+    // let indexOfLastTodo = currentPage * todosPerPage; 
+    // let indexOfFirstTodo = indexOfLastTodo - todosPerPage; 
+    // let currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo); 
+    // prev = currentPage > 0 ? (currentPage -1) :0; 
+    // last = Math.ceil(todos.length/todosPerPage); 
+    // next = (last === currentPage) ?currentPage: currentPage +1; 
+    
+    // Logic for displaying page numbers 
+    let pageNumbers = []; 
+    for (let i = 1; i <=last; i++) { 
+      pageNumbers.push(i); 
+    } 
+    return ( 
+      <div className="animated fadeIn"> 
+      <Row> 
+      <Col> 
+      <Card> 
+      <CardHeader> 
+      <i className="fa fa-align-justify"></i> 充值记录 
+      </CardHeader> 
+      <CardBody> 
+      <SearchDate 
+      callApi={this.callApi} 
+      onSetList={this.handleSetList} /> 
+      
+      <TableData list={this.state.list} /> 
+      {/* <ul> 
+      { 
+        currentTodos.map((todo,index) =>{ 
+          return <li key={index}>{todo}</li>; 
+        }) 
+      } 
+      </ul> */}
+      <ul id="page-numbers"> 
+      <nav> 
+      <Pagination> 
+        <PaginationItem> 
+        { prev === 0 ? <PaginationLink disabled>First</PaginationLink> : 
+          <PaginationLink 
+            onClick={this.handleFirstClick} 
+            id={prev} 
+            href={prev}>First
+          </PaginationLink> 
+        } 
+        </PaginationItem> 
+        <PaginationItem> 
+        { prev === 0 ? <PaginationLink disabled>Prev</PaginationLink> : 
+            <PaginationLink 
+              onClick={this.handleClick} 
+              id={prev} href={prev}>Prev
+            </PaginationLink> 
+        } 
+        </PaginationItem> 
+        { 
+          pageNumbers.map((number,i) => 
+          <Pagination key= {i}> 
+            <PaginationItem 
+            active = {pageNumbers[currentPage-1] === (number) ? true : false} > 
+              <PaginationLink 
+                onClick={this.handleClick} 
+                href={number} key={number} id={number}> 
+                {number} 
+              </PaginationLink> 
+            </PaginationItem> 
+          </Pagination> 
+          )} 
+          
+          <PaginationItem> 
+          { 
+            currentPage === last ? <PaginationLink disabled>Next</PaginationLink> : 
+              <PaginationLink 
+                onClick={this.handleClick} 
+                id={pageNumbers[currentPage]} 
+                href={pageNumbers[currentPage]}>Next
+              </PaginationLink> 
+          } 
+          </PaginationItem> 
+          
+          <PaginationItem> 
+          { 
+            currentPage === last ? <PaginationLink disabled>Last</PaginationLink> : 
+              <PaginationLink 
+                onClick={this.handleLastClick} 
+                id={pageNumbers[currentPage]} 
+                href={pageNumbers[currentPage]}>Last
+              </PaginationLink> 
+          } 
+          </PaginationItem> 
+        </Pagination> 
+        </nav> 
+        </ul> 
+        </CardBody> 
+        </Card> 
+        </Col> 
+        </Row> 
+        </div> 
+        ) 
+      } 
     }
-}
